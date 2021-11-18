f8f8be3bf139526e291b0a36c4ca5ebc250928a5
KarlGNassar <kgnassar01@gmail.com>
2021-11-18 16:46:35 +0200

    utility classes

--- playgrounds/react/dist/src.f69400ca.js
+++ playgrounds/react/dist/src.f69400ca.js
@@ -29566,7 +29566,36 @@ if ("development" === 'production') {
 } else {
   module.exports = require('./cjs/react-dom.development.js');
 }
-},{"./cjs/react-dom.development.js":"../../../node_modules/react-dom/cjs/react-dom.development.js"}],"../../../node_modules/@advanced-react/react/lib/atoms/Color/Color.js":[function(require,module,exports) {
+},{"./cjs/react-dom.development.js":"../../../node_modules/react-dom/cjs/react-dom.development.js"}],"../../../node_modules/@advanced-react/react/lib/foundation/Spacing.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+const spaces = {
+  xxxs: "xxxs",
+  // 4px
+  xxs: "xxs",
+  // 8px
+  xs: "xs",
+  // 12px
+  sm: "sm",
+  // 16px
+  md: "md",
+  // 24px
+  lg: "lg",
+  // 32px
+  xl: "xl",
+  // 48px
+  xxl: "xxl",
+  // 72px
+  xxxl: "xxxl" // 96px
+
+};
+var Spacing = Object.freeze(spaces);
+exports.default = Spacing;
+},{}],"../../../node_modules/@advanced-react/react/lib/atoms/Color/Color.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -29576,24 +29605,26 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _Spacing = _interopRequireDefault(require("../../foundation/Spacing.js"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const Color = ({
   hexCode,
-  width,
-  height
+  width = _Spacing.default.sm,
+  height = _Spacing.default.sm
 }) => {
+  const className = `dse-width-${width} dse-height-${height}`;
   return _react.default.createElement("div", {
+    className: className,
     style: {
-      backgroundColor: hexCode,
-      width,
-      height
+      backgroundColor: hexCode
     }
   });
 };
 
 exports.default = Color;
-},{"react":"../../../node_modules/react/index.js"}],"../../../node_modules/@advanced-react/react/lib/index.js":[function(require,module,exports) {
+},{"react":"../../../node_modules/react/index.js","../../foundation/Spacing.js":"../../../node_modules/@advanced-react/react/lib/foundation/Spacing.js"}],"../../../node_modules/@advanced-react/react/lib/index.js":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -29605,11 +29636,19 @@ Object.defineProperty(exports, "Color", {
     return _Color.default;
   }
 });
+Object.defineProperty(exports, "Spacing", {
+  enumerable: true,
+  get: function () {
+    return _Spacing.default;
+  }
+});
 
 var _Color = _interopRequireDefault(require("./atoms/Color/Color.js"));
 
+var _Spacing = _interopRequireDefault(require("./foundation/Spacing.js"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-},{"./atoms/Color/Color.js":"../../../node_modules/@advanced-react/react/lib/atoms/Color/Color.js"}],"../../../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+},{"./atoms/Color/Color.js":"../../../node_modules/@advanced-react/react/lib/atoms/Color/Color.js","./foundation/Spacing.js":"../../../node_modules/@advanced-react/react/lib/foundation/Spacing.js"}],"../../../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
@@ -29682,6 +29721,12 @@ module.exports = reloadCSS;
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
+},{"_css_loader":"../../../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../node_modules/@advanced-react/scss/lib/Utilities.css":[function(require,module,exports) {
+
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
 },{"_css_loader":"../../../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.tsx":[function(require,module,exports) {
 "use strict";
 
@@ -29693,14 +29738,16 @@ var _react2 = require("@advanced-react/react");
 
 require("@advanced-react/scss/lib/Button.css");
 
+require("@advanced-react/scss/lib/Utilities.css");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _reactDom.default.render(_react.default.createElement(_react2.Color, {
   hexCode: "#000",
-  width: "1rem",
-  height: "1rem"
+  width: "lg",
+  height: "lg"
 }), document.querySelector("#root"));
-},{"react":"../../../node_modules/react/index.js","react-dom":"../../../node_modules/react-dom/index.js","@advanced-react/react":"../../../node_modules/@advanced-react/react/lib/index.js","@advanced-react/scss/lib/Button.css":"../../../node_modules/@advanced-react/scss/lib/Button.css"}],"../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"react":"../../../node_modules/react/index.js","react-dom":"../../../node_modules/react-dom/index.js","@advanced-react/react":"../../../node_modules/@advanced-react/react/lib/index.js","@advanced-react/scss/lib/Button.css":"../../../node_modules/@advanced-react/scss/lib/Button.css","@advanced-react/scss/lib/Utilities.css":"../../../node_modules/@advanced-react/scss/lib/Utilities.css"}],"../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -29728,7 +29775,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59147" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61595" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 
 import { Color } from "@advanced-react/react";
 import "@advanced-react/scss/lib/Button.css";
+import "@advanced-react/scss/lib/Utilities.css";
 
 ReactDOM.render(
-  <Color hexCode="#000" width="1rem" height="1rem" />,
+  <Color hexCode="#000" width="lg" height="lg" />,
   document.querySelector("#root"),
 );
